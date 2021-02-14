a8eabb2c72a931497b7ed76817cbcd0e1c9fd0a2
Chi Chow <cschow@ucsd.edu>
2021-02-13 16:01:47 -0800

    fixed typo image

--- scripts/script.js
+++ scripts/script.js
@@ -3,34 +3,34 @@
 let myStorage = window.localStorage;
 
 window.addEventListener('DOMContentLoaded', () => {
-  let products = myStorage.getItem('products');
-  if (products === null) {
-    fetch("https://fakestoreapi.com/products")
-      .then(response => {
-        myStorage.setItem('products', JSON.stringify(response));
-        products = JSON.parse(myStorage.getItem('products'));
+    let products = myStorage.getItem('products');
+    if (products === null) {
+        fetch("https://fakestoreapi.com/products")
+            .then(response => {
+                myStorage.setItem('products', JSON.stringify(response));
+                products = JSON.parse(myStorage.getItem('products'));
+                updateProducts(products);
+            })
+            .catch(error => {
+                console.log("Chi is trolling me");
+            });
+    } else {
         updateProducts(products);
-      })
-      .catch(error => {
-        console.log("Chi is trolling me");
-      });
-  } else {
-    updateProducts(products);
-  }
+    }
 });
 
 function updateProducts(products) {
-  const productList = document.getElementById('product-list');
-  console.log(products)
-  for (index in products) {
-    let product = products[index];
-    console.log(product);
+    const productList = document.getElementById('product-list');
+    console.log(products)
+    for (index in products) {
+        let product = products[index];
+        console.log(product);
 
-    let comp = document.createElement('product-item');
-    comp.setAttribute('img', product.image);
-    comp.setAttribute('title', product.title);
-    comp.setAttribute('price', product.price);
+        let comp = document.createElement('product-item');
+        comp.setAttribute('iamge', product.image);
+        comp.setAttribute('title', product.title);
+        comp.setAttribute('price', product.price);
 
-    productList.appendChild(comp);
-  }
+        productList.appendChild(comp);
+    }
 }
